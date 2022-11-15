952da826cdd8e0e09ba6ff57c6ab9b9056bf76cd
Axel Nana <axel.nana@aliens-group.com>
2022-11-15 05:00:45 +0100

    Add spark-lang as a third party dependency.

---
+++ Code/ThirdParty/spark/init.cmake
@@ -0,0 +1,4 @@
+### SparkLang
+set (EZ_3RDPARTY_SPARKLANG_SUPPORT ON CACHE BOOL "Whether to add support for SparkLang.")
+set (SPARKLANG_ENABLE_VAR_TRACE OFF CACHE BOOL "Whether to enable var trace in SparkLang.")
+mark_as_advanced(FORCE EZ_3RDPARTY_SPARKLANG_SUPPORT)
