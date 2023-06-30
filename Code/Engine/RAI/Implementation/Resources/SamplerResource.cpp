b96bd1e8d17135532032a5bc99e450b02268b6c0
Axel Nana <axel.nana@aliens-group.com>
2023-06-30 04:32:12 +0100

    Add cpp files to EZ_STATICLINK_LIBRARY.
    Signed-off-by: Axel Nana <axel.nana@aliens-group.com>

--- Code/Engine/RAI/Implementation/Resources/SamplerResource.cpp
+++ Code/Engine/RAI/Implementation/Resources/SamplerResource.cpp
@@ -1,4 +1,18 @@
-﻿#include <RAI/RAIPCH.h>
+﻿// Copyright (c) 2023-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <RAI/RAIPCH.h>
 
 #include <RAI/Resources/SamplerResource.h>
 
@@ -153,3 +167,5 @@ namespace RAI
 #pragma endregion
 
 } // namespace RAI
+
+EZ_STATICLINK_FILE(RAI, RAI_Implementation_Resources_SamplersResource);
