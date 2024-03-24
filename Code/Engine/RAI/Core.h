907b91a659461614a85be4ba52bb82d06e3cf22c
Axel Nana <axel.nana@aliens-group.com>
2024-03-24 11:09:57 +0100

    chore(RPI): Remove old `spVertex` struct.

--- Code/Engine/RAI/Core.h
+++ Code/Engine/RAI/Core.h
@@ -27,47 +27,6 @@ enum SP_RAI_CONSTANTS : ezUInt8
 
 namespace RAI
 {
-  /// \brief A single vertex in a 3D mesh.
-  struct spVertex
-  {
-    EZ_DECLARE_POD_TYPE();
-    EZ_DECLARE_MEM_RELOCATABLE_TYPE();
-
-    /// \brief The vertex position.
-    ezVec3 m_vPosition;
-
-    /// \brief The vertex normal.
-    ezVec3 m_vNormal;
-
-    /// \brief The vertex tangent.
-    ezVec4 m_vTangent;
-
-    /// \brief The vertex bitangent.
-    ezVec4 m_vBiTangent;
-
-    /// \brief The vertex texture coordinates at channel 1.
-    ezVec2 m_vTexCoord0;
-
-    /// \brief The vertex texture coordinates at channel 2.
-    ezVec2 m_vTexCoord1;
-
-    /// \brief The vertex color at channel 1.
-    ezColorLinearUB m_Color0;
-
-    /// \brief The vertex color at channel 2.
-    ezColorLinearUB m_Color1;
-
-    EZ_NODISCARD EZ_ALWAYS_INLINE bool operator==(const spVertex& other) const
-    {
-      return m_vPosition == other.m_vPosition && m_vNormal == other.m_vNormal && m_vTangent == other.m_vTangent && m_vBiTangent == other.m_vBiTangent && m_vTexCoord0 == other.m_vTexCoord0 && m_vTexCoord1 == other.m_vTexCoord1 && m_Color0 == other.m_Color0 && m_Color1 == other.m_Color1;
-    }
-
-    EZ_NODISCARD EZ_ALWAYS_INLINE bool operator!=(const spVertex& other) const
-    {
-      return !(*this == other);
-    }
-  };
-
   /// \biref A permutation variable in a shader variant.
   struct SP_RAI_DLL spPermutationVar
   {
@@ -81,34 +40,6 @@ namespace RAI
 
     EZ_ALWAYS_INLINE bool operator==(const spPermutationVar& other) const { return m_sName == other.m_sName && m_sValue == other.m_sValue; }
   };
-
-  EZ_FORCE_INLINE ezStreamWriter& operator<<(ezStreamWriter& inout_stream, const spVertex& vertex)
-  {
-    inout_stream << vertex.m_vPosition;
-    inout_stream << vertex.m_vNormal;
-    inout_stream << vertex.m_vTangent;
-    inout_stream << vertex.m_vBiTangent;
-    inout_stream << vertex.m_vTexCoord0;
-    inout_stream << vertex.m_vTexCoord1;
-    inout_stream << vertex.m_Color0;
-    inout_stream << vertex.m_Color1;
-
-    return inout_stream;
-  }
-
-  EZ_FORCE_INLINE ezStreamReader& operator>>(ezStreamReader& inout_stream, spVertex& ref_vertex)
-  {
-    inout_stream >> ref_vertex.m_vPosition;
-    inout_stream >> ref_vertex.m_vNormal;
-    inout_stream >> ref_vertex.m_vTangent;
-    inout_stream >> ref_vertex.m_vBiTangent;
-    inout_stream >> ref_vertex.m_vTexCoord0;
-    inout_stream >> ref_vertex.m_vTexCoord1;
-    inout_stream >> ref_vertex.m_Color0;
-    inout_stream >> ref_vertex.m_Color1;
-
-    return inout_stream;
-  }
 
   EZ_FORCE_INLINE ezStreamWriter& operator<<(ezStreamWriter& inout_stream, const spPermutationVar& permutation)
   {
