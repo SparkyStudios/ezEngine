5975edb917d098f9310a315eb0471cf58e64f09d
Axel Nana <axel.nana@aliens-group.com>
2024-03-29 11:50:53 +0100

    wip(RPI): Add a sample project to test the RPI architecture.
    Signed-off-by: Axel Nana <axel.nana@aliens-group.com>

---
+++ Code/Samples/RPISample/RPIGameApplication.cpp
@@ -0,0 +1,413 @@
+#include "RPIGameApplication.h"
+
+#include <Core/Input/DeviceTypes/Controller.h>
+#include <Core/ResourceManager/ResourceManager.h>
+#include <Core/System/ControllerInput.h>
+#include <Core/System/Window.h>
+#include <Core/World/World.h>
+
+#include <Foundation/Configuration/CVar.h>
+#include <Foundation/IO/FileSystem/DataDirTypeFolder.h>
+#include <Foundation/IO/FileSystem/FileSystem.h>
+#include <Foundation/Time/Clock.h>
+
+#include <RHI/Core.h>
+#include <RHI/Factory.h>
+
+#include <RPI/Camera/CameraComponent.h>
+#include <RPI/Graph/Nodes/MainSwapchainRenderGraphNode.h>
+#include <RPI/Meshes/MeshComponent.h>
+#include <RPI/Pipeline/RenderPass.h>
+
+#if EZ_ENABLED(EZ_PLATFORM_WINDOWS_DESKTOP)
+#  include <RHID3D11/Device.h>
+#  include <RHID3D11/ResourceManager.h>
+#  include <RHID3D11/Swapchain.h>
+#elif EZ_ENABLED(EZ_PLATFORM_OSX)
+#  include <RHIMTL/Device.h>
+#  include <RHIMTL/Swapchain.h>
+#endif
+
+static ezUInt32 g_uiWindowWidth = 640;
+static ezUInt32 g_uiWindowHeight = 480;
+
+ezResult spDemoRenderGraphNode::Setup(spRenderGraphBuilder* pBuilder, const ezHashTable<ezHashedString, spResourceHandle>& resources)
+{
+  spRenderTargetDescription rtDescription{};
+  rtDescription.m_bGenerateMipMaps = false;
+  rtDescription.m_eQuality = spRenderTargetQuality::LDR;
+  rtDescription.m_eSampleCount = spTextureSampleCount::None;
+  rtDescription.m_uiHeight = g_uiWindowHeight;
+  rtDescription.m_uiWidth = g_uiWindowWidth;
+
+  m_hRenderTarget = pBuilder->CreateRenderTarget(this, rtDescription, spRenderGraphResourceBindType::ReadOnly);
+
+  return EZ_SUCCESS;
+}
+
+ezUniquePtr<spRenderPass> spDemoRenderGraphNode::Compile(spRenderGraphBuilder* pBuilder)
+{
+  spRenderGraphResource* rtt = nullptr;
+  pBuilder->GetResources().TryGetValue(m_hRenderTarget.GetInternalID(), rtt);
+  rtt->m_pRHIResource->SetDebugName("main_render_target");
+
+  auto target = m_hRenderTarget;
+  const spCallbackRenderPass::ExecuteCallback executeCallback = [target](const spRenderGraphResourcesTable& resources, spRenderContext* context, ezVariant& passData) -> void
+  {
+    spRenderGraphResource* rtt = nullptr;
+    resources.TryGetValue(target.GetInternalID(), rtt);
+
+    const auto cl = context->GetCommandList();
+
+    const auto c = ezAngle::MakeFromRadian(ezTime::Now().AsFloatInSeconds());
+    const auto col = ezColor(ezMath::Sin(c), ezMath::Cos(c), ezMath::Sin(-c), 1.0f);
+
+    ezSharedPtr<spScopeProfiler> pTestScopeProfiler;
+
+    cl->PushProfileScope("DemoNode");
+    {
+      cl->SetFramebuffer(rtt->m_pRHIResource.Downcast<spRenderTarget>()->GetFramebuffer());
+
+      cl->SetFullViewport(0);
+      cl->SetFullScissorRect(0);
+
+      cl->ClearColorTarget(0, col);
+    }
+    cl->PopProfileScope(pTestScopeProfiler);
+  };
+
+  const spCallbackRenderPass::CleanUpCallback cleanUpCallback = [target](const spRenderGraphResourcesTable& resources, ezVariant& passData) -> void
+  {
+    spRenderGraphResource* rtt = nullptr;
+    resources.TryGetValue(target.GetInternalID(), rtt);
+    rtt->m_pRHIResource.Clear();
+  };
+
+  ezUniquePtr<spRenderPass> pPass = EZ_NEW(pBuilder->GetAllocator(), spCallbackRenderPass, executeCallback, cleanUpCallback);
+
+  return pPass;
+}
+
+bool spDemoRenderGraphNode::IsEnabled() const
+{
+  return true;
+}
+
+class spRPISampleWindow : public ezWindow
+{
+public:
+  spRPISampleWindow(spRPIGameApplication* pApp)
+    : ezWindow()
+  {
+    m_pApp = pApp;
+    m_bCloseRequested = false;
+  }
+
+  void OnClickClose() override { m_bCloseRequested = true; }
+
+  void OnResize(const ezSizeU32& newWindowSize) override
+  {
+    if (m_pApp)
+    {
+      m_CreationDescription.m_Resolution = newWindowSize;
+      m_pApp->OnResize(m_CreationDescription.m_Resolution.width, m_CreationDescription.m_Resolution.height);
+    }
+  }
+
+  bool m_bCloseRequested;
+
+private:
+  spRPIGameApplication* m_pApp = nullptr;
+};
+
+spRPIGameApplication::spRPIGameApplication()
+  : ezGameApplication("RPI Sample", "Data/Samples/RHISample")
+{
+}
+
+void spRPIGameApplication::AfterCoreSystemsStartup()
+{
+  ExecuteInitFunctions();
+
+  ezWorldDesc desc("RPISampleWorld");
+  desc.m_uiRandomNumberGeneratorSeed = 42;
+
+  m_pWorld = EZ_DEFAULT_NEW(ezWorld, desc);
+  m_pWorld->GetClock().SetFixedTimeStep(ezTime::MakeFromSeconds(1.0 / 60.0));
+
+  m_pSceneContext = ezUniquePtr<spSceneContext>(
+    spRenderSystem::GetSingleton()->CreateSceneForWorld(m_pWorld.Borrow()),
+    spDeviceAllocatorWrapper::GetAllocator());
+
+  //  ActivateGameState(m_pWorld.Borrow()).IgnoreResult();
+
+  ezUniquePtr<spRenderGraphBuilder> builder = EZ_NEW(spDeviceAllocatorWrapper::GetAllocator(), spRenderGraphBuilder, spRenderSystem::GetSingleton()->GetDevice().Borrow());
+
+  {
+    ezUniquePtr<spDemoRenderGraphNode> triangleNode = EZ_NEW(spDeviceAllocatorWrapper::GetAllocator(), spDemoRenderGraphNode);
+    builder->AddNode("DemoNode", std::move(triangleNode));
+  }
+
+  {
+    ezUniquePtr<spMainSwapchainRenderGraphNode> swapchainNode = EZ_NEW(spDeviceAllocatorWrapper::GetAllocator(), spMainSwapchainRenderGraphNode);
+    swapchainNode->SetRenderTargetSize(g_uiWindowWidth, g_uiWindowHeight);
+
+    ezHashTable<ezHashedString, spResourceHandle> swapchainNodeResources;
+    swapchainNodeResources.Insert(ezMakeHashedString("Input"), static_cast<const spDemoRenderGraphNode*>(builder->GetNode("DemoNode"))->GetTarget());
+
+    builder->AddNode("Swapchain", std::move(swapchainNode), swapchainNodeResources);
+  }
+
+  m_pSceneContext->AddPipeline(builder->Compile());
+
+  {
+    EZ_LOCK(m_pWorld->GetWriteMarker());
+
+    {
+      ezGameObjectDesc cubeDesc;
+      cubeDesc.m_sName.Assign("Cube");
+      m_pWorld->CreateObject(cubeDesc, m_pCube);
+
+      m_pWorld->GetOrCreateComponentManager<spMeshComponentManager>()->CreateComponent(m_pCube);
+    }
+
+    {
+      ezGameObjectDesc cameraDesc;
+      cameraDesc.m_sName.Assign("Camera");
+      m_pWorld->CreateObject(cameraDesc, m_pCamera);
+
+      m_pWorld->GetOrCreateComponentManager<spCameraComponentManager>()->CreateComponent(m_pCamera);
+    }
+  }
+}
+
+void spRPIGameApplication::BeforeHighLevelSystemsShutdown()
+{
+  spRenderSystem::GetSingleton()->UnregisterSceneForWorld(m_pWorld.Borrow());
+  m_pWorld = nullptr;
+
+  ezGameApplicationBase::BeforeHighLevelSystemsShutdown();
+
+  m_pSceneContext->CleanUp();
+}
+
+void spRPIGameApplication::OnResize(ezUInt32 width, ezUInt32 height)
+{
+  spRenderSystem::GetSingleton()->GetDevice()->ResizeSwapchain(width, height);
+}
+
+void spRPIGameApplication::Init_LoadRequiredPlugins()
+{
+  ezPlugin::InitializeStaticallyLinkedPlugins();
+
+#if EZ_ENABLED(EZ_COMPILE_FOR_DEVELOPMENT)
+  ezPlugin::LoadPlugin("ezInspectorPlugin").IgnoreResult();
+#endif
+}
+
+void spRPIGameApplication::Init_SetupDefaultResources()
+{
+  // noop
+}
+
+void spRPIGameApplication::Init_SetupGraphicsDevice()
+{
+  ezEnum<spGraphicsApi> eApiType;
+
+  ezStringView szRendererName = ezCommandLineUtils::GetGlobalInstance()->GetStringOption("-rhi", 0, "MTL");
+  {
+    if (szRendererName.Compare("D3D11") == 0)
+    {
+      eApiType = spGraphicsApi::Direct3D11;
+    }
+
+    else if (szRendererName.Compare("D3D12") == 0)
+    {
+      eApiType = spGraphicsApi::Direct3D12;
+    }
+
+    else if (szRendererName.Compare("VK") == 0)
+    {
+      eApiType = spGraphicsApi::Vulkan;
+    }
+
+    else if (szRendererName.Compare("GL") == 0)
+    {
+      eApiType = spGraphicsApi::OpenGL;
+    }
+
+    else if (szRendererName.Compare("GLES") == 0)
+    {
+      eApiType = spGraphicsApi::OpenGLES;
+    }
+
+    else if (szRendererName.Compare("MTL") == 0)
+    {
+      eApiType = spGraphicsApi::Metal;
+    }
+  }
+
+  // Create a window for rendering
+  {
+    ezWindowCreationDesc WindowCreationDesc;
+    WindowCreationDesc.m_Resolution.width = g_uiWindowWidth;
+    WindowCreationDesc.m_Resolution.height = g_uiWindowHeight;
+    WindowCreationDesc.m_Title = ezStringBuilder("RPISample ", szRendererName);
+    WindowCreationDesc.m_bShowMouseCursor = true;
+    WindowCreationDesc.m_bClipMouseCursor = false;
+    WindowCreationDesc.m_WindowMode = ezWindowMode::WindowResizable;
+    m_pWindow = EZ_DEFAULT_NEW(spRPISampleWindow, this);
+    m_pWindow->Initialize(WindowCreationDesc).IgnoreResult();
+  }
+
+  // Init the rendering system
+  {
+    spRenderSystem* pRenderSystem = EZ_DEFAULT_NEW(RPI::spRenderSystem);
+
+    if (eApiType == spGraphicsApi::Direct3D11)
+    {
+#if EZ_ENABLED(EZ_PLATFORM_WINDOWS_DESKTOP)
+      spRenderingSurfaceWin32 renderSurface(ezMinWindows::ToNative(m_pWindow->GetNativeWindowHandle()), nullptr, m_pWindow->IsFullscreenWindow());
+
+      spDeviceDescriptionD3D11 description{};
+      description.m_bDebug = true;
+      description.m_bSyncV = false;
+      description.m_bUsSrgbFormat = true;
+      description.m_uiWidth = g_uiWindowWidth;
+      description.m_uiHeight = g_uiWindowHeight;
+      description.m_bHasMainSwapchain = true;
+      description.m_MainSwapchainDescription.m_uiWidth = g_uiWindowWidth;
+      description.m_MainSwapchainDescription.m_uiHeight = g_uiWindowHeight;
+      description.m_MainSwapchainDescription.m_bUseSrgb = false;
+      description.m_MainSwapchainDescription.m_bVSync = false;
+      description.m_MainSwapchainDescription.m_bUseDepthTexture = true;
+      description.m_MainSwapchainDescription.m_pRenderingSurface = &renderSurface;
+      description.m_MainSwapchainDescription.m_eDepthFormat = spPixelFormat::D24UNormS8UInt;
+      description.m_bMainSwapchainHasDepth = true;
+      description.m_eSwapchainDepthPixelFormat = spPixelFormat::D24UNormS8UInt;
+      description.m_bPreferDepthRangeZeroToOne = true;
+
+      auto pDevice = spRHIImplementationFactory::CreateDevice(szRendererName, ezDefaultAllocatorWrapper::GetAllocator(), description);
+      pRenderSystem->Startup(pDevice);
+#else
+      EZ_ASSERT_ALWAYS(false, "Direct3D11 is not supported on this platform.");
+#endif
+    }
+    else if (eApiType == spGraphicsApi::Metal)
+    {
+#if EZ_ENABLED(EZ_PLATFORM_OSX)
+      spRenderingSurfaceNSWindow renderSurface(m_pWindow->GetNativeWindowHandle());
+
+      spDeviceDescription description{};
+      description.m_bDebug = true;
+      description.m_bSyncV = false;
+      description.m_bUsSrgbFormat = true;
+      description.m_uiWidth = g_uiWindowWidth;
+      description.m_uiHeight = g_uiWindowHeight;
+      description.m_bHasMainSwapchain = true;
+      description.m_MainSwapchainDescription.m_uiWidth = g_uiWindowWidth;
+      description.m_MainSwapchainDescription.m_uiHeight = g_uiWindowHeight;
+      description.m_MainSwapchainDescription.m_bUseSrgb = false;
+      description.m_MainSwapchainDescription.m_bVSync = false;
+      description.m_MainSwapchainDescription.m_bUseDepthTexture = true;
+      description.m_MainSwapchainDescription.m_pRenderingSurface = &renderSurface;
+      description.m_MainSwapchainDescription.m_eDepthFormat = spPixelFormat::D32FloatS8UInt;
+      description.m_bMainSwapchainHasDepth = true;
+      description.m_eSwapchainDepthPixelFormat = spPixelFormat::D32FloatS8UInt;
+      description.m_bPreferDepthRangeZeroToOne = true;
+
+      auto pDevice = spRHIImplementationFactory::CreateDevice(szRendererName, ezDefaultAllocatorWrapper::GetAllocator(), description);
+      pRenderSystem->Startup(pDevice);
+#else
+      EZ_ASSERT_ALWAYS(false, "Metal is not supported on this platform.");
+#endif
+    }
+
+    EZ_ASSERT_DEV(pRenderSystem->GetDevice() != nullptr, "Device creation failed");
+  }
+}
+
+void spRPIGameApplication::Deinit_ShutdownGraphicsDevice()
+{
+  // Release all RHI resources
+  spRenderSystem::GetSingleton()->GetDevice()->GetResourceManager()->ReleaseResources();
+
+  spRenderSystem::GetSingleton()->UnregisterSceneForWorld(m_pWorld.Borrow());
+  m_pSceneContext = nullptr;
+
+  spRenderSystem::GetSingleton()->Shutdown();
+
+  m_pWindow->Destroy().IgnoreResult();
+  m_pWindow = nullptr;
+}
+
+ezApplication::Execution spRPIGameApplication::Run()
+{
+  m_pWindow->ProcessWindowMessages();
+
+  if (m_pWindow->m_bCloseRequested)
+    return Execution::Quit;
+
+  return SUPER::Run();
+}
+
+bool spRPIGameApplication::IsGameUpdateEnabled() const
+{
+  return true;
+}
+
+bool spRPIGameApplication::Run_ProcessApplicationInput()
+{
+  return SUPER::Run_ProcessApplicationInput();
+}
+
+void spRPIGameApplication::Run_WorldUpdateAndRender()
+{
+  EZ_PROFILE_SCOPE("Run_WorldUpdateAndRender");
+
+  m_pSceneContext->Collect();
+
+  {
+    Run_BeforeWorldUpdate();
+    {
+      EZ_LOCK(m_pWorld->GetWriteMarker());
+      m_pWorld->Update();
+    }
+    Run_AfterWorldUpdate();
+
+    Run_UpdatePlugins();
+  }
+
+  if (spRenderSystem::IsMultiThreadedRendering())
+    spRenderSystem::GetSingleton()->GetRenderThread()->PostAsync(ezMakeDelegate(&spSceneContext::BeginFrame, m_pSceneContext.Borrow()));
+  else
+    m_pSceneContext->BeginFrame();
+
+  if (spRenderSystem::IsMultiThreadedRendering())
+    spRenderSystem::GetSingleton()->GetRenderThread()->PostAsync(ezMakeDelegate(&spSceneContext::Draw, m_pSceneContext.Borrow()));
+  else
+    m_pSceneContext->Draw();
+}
+
+void spRPIGameApplication::Run_Present()
+{
+  EZ_PROFILE_SCOPE("Run_Present");
+  if (spRenderSystem::IsMultiThreadedRendering())
+    spRenderSystem::GetSingleton()->GetRenderThread()->PostAsync(ezMakeDelegate(&spSceneContext::Present, m_pSceneContext.Borrow()));
+  else
+    m_pSceneContext->Present();
+}
+
+void spRPIGameApplication::Run_FinishFrame()
+{
+  if (spRenderSystem::IsMultiThreadedRendering())
+    spRenderSystem::GetSingleton()->GetRenderThread()->PostAsync(ezMakeDelegate(&spSceneContext::EndFrame, m_pSceneContext.Borrow()));
+  else
+    m_pSceneContext->EndFrame();
+
+  m_pSceneContext->WaitForIdle();
+
+  ezGameApplicationBase::Run_FinishFrame();
+}
+
+EZ_APPLICATION_ENTRY_POINT(spRPIGameApplication);
---
+++ Code/Samples/RPISample/RPIGameApplication.h
@@ -0,0 +1,85 @@
+// Copyright (c) 2024-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <GameEngine/GameApplication/GameApplication.h>
+
+#include <RPI/Core/RenderSystem.h>
+#include <RPI/Core/Threading/RenderThread.h>
+#include <RPI/Graph/RenderGraph.h>
+#include <RPI/Scene/SceneContext.h>
+
+using namespace RHI;
+using namespace RPI;
+
+class spRPISampleWindow;
+
+class spRPIGameApplication : public ezGameApplication
+{
+  using SUPER = ezGameApplication;
+
+public:
+  spRPIGameApplication();
+
+  void AfterCoreSystemsStartup() override;
+  void BeforeHighLevelSystemsShutdown() override;
+
+  void OnResize(ezUInt32 width, ezUInt32 height);
+
+protected:
+  void Init_LoadRequiredPlugins() override;
+  void Init_SetupDefaultResources() override;
+  void Init_SetupGraphicsDevice() override;
+  void Deinit_ShutdownGraphicsDevice() override;
+
+
+  ezApplication::Execution Run() override;
+  bool IsGameUpdateEnabled() const override;
+
+  bool Run_ProcessApplicationInput() override;
+  void Run_WorldUpdateAndRender() override;
+  void Run_Present() override;
+  void Run_FinishFrame() override;
+
+private:
+  ezGameObject* m_pCube{nullptr};
+  ezGameObject* m_pCamera{nullptr};
+
+  ezUniquePtr<ezWorld> m_pWorld{nullptr};
+  ezUniquePtr<spSceneContext> m_pSceneContext{nullptr};
+
+  ezUniquePtr<spRPISampleWindow> m_pWindow{nullptr};
+};
+
+
+class spDemoRenderGraphNode final : public spRenderGraphNode
+{
+public:
+  spDemoRenderGraphNode()
+    : spRenderGraphNode("DemoRenderGraphNode")
+  {
+  }
+
+  ezResult Setup(spRenderGraphBuilder* pBuilder, const ezHashTable<ezHashedString, spResourceHandle>& resources) override;
+
+  ezUniquePtr<spRenderPass> Compile(spRenderGraphBuilder* pBuilder) override;
+
+  bool IsEnabled() const override;
+
+  EZ_NODISCARD EZ_ALWAYS_INLINE spResourceHandle GetTarget() const { return m_hRenderTarget; }
+
+private:
+  spResourceHandle m_hRenderTarget;
+};
